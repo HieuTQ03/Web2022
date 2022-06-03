2a80aa7bdb255c9c73feed70960f33b14e817498
HieuTQ03 <truongquochieu2003@gmail.com>
2022-06-03 22:42:43 +0700

    update db

--- BaiTap3/home.js
+++ BaiTap3/home.js
@@ -4,6 +4,7 @@ const data=[
     {id:1 ,img:"images/Mask Group (2).png",title:"The Best Easy One Pot Chicken and Rice"},
     {id:1 ,img:"images/Mask Group (3).png",title:"Fresh and Healthy Mixed Mayonnaise"},
     {id:1 ,img:"images/Mask Group(4).png",title:"The Creamiest Creamy Chicken"},
-    {id:1 ,img:"images/Mask Group(5).png",title:"Fruity Pancake with Orange & Blueberry"}
+    {id:1 ,img:"images/Mask Group(5).png",title:"Fruity Pancake with Orange & Blueberry"},
 ];
+
 
--- BaiTap4/main.js
+++ BaiTap4/main.js
@@ -1,17 +1,19 @@
 import { render } from "./common";
-import{categories,productList } from "./data";
-function showData(data,key){
-    if(!Array.isArray(data)|| data.length==0) return false;
+import { categories, productList } from "./data";
 
-    let result ="";
-    if(key =="product"){
-        {
-            for(let i=0;i<data.length;i++){
-                result+=<h2><a href="./detail.html?id=${data[i].id}">${data[i].name}</a></h2>
-            }
+function showData(data, key) {
+    // kiểm tra
+    if (!Array.isArray(data) || data.length == 0) return false;
+    // xử lý
+    let result = "";
+    if (key == "product") {
+        for (let i = 0; i < data.length; i++) {
+            result += `<h2><a href="./detail.html?id=${data[i].id}">${data[i].name}</a></h2>`;
+        }
+    } else {
+        for (let i = 0; i < data.length; i++) {
+            result += `<div><h2><a href="./product.html?id=${data[i].id}">${data[i].name}</a></h2></div>`;
         }
     }
     return result;
 }
-render("product", showData(productList, "product"));
-render("category", showData(categories, "category"));
